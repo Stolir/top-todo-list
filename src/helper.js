252733aecfa194fce9ec1ef57518b5f501ec7f3c
Abdelrahman <119765066+Stolir@users.noreply.github.com>
2025-02-04 14:57:34 +0300

    Fix logic error within "ImportAll"

--- src/helper.js
+++ src/helper.js
@@ -1,8 +1,7 @@
 // imports all files (images/svgs/pngs etc) and maps them to a directory
 export function importAll (directory) {
-    const directory = require.context(directory, false, /\.(png|jpe?g|svg)$/);
     let images = {};
-  directory.keys().map((item) => {
+    directory.keys().map((item) => {
     images[item.replace('./', '')] = directory(item);
   });
   return images;
