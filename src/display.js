199a3e4852910ef8a5360e7de20e02ebbb8fd1d9
Abdelrahman <119765066+Stolir@users.noreply.github.com>
2025-02-07 10:37:53 +0300

    Write IIFE to make cards of different types

--- src/display.js
+++ src/display.js
@@ -1,3 +1,5 @@
+import { Task, Note } from "./filters";
+
 const cardContainer = document.querySelector("#main");
 const sidebarElement = document.querySelectorAll("#sidebar > div")
 
@@ -6,15 +8,7 @@ const sidebarElement = document.querySelectorAll("#sidebar > div")
     cardContainer.textContent = "";
     
     for (let filter of filters) {
-        const card = document.createElement("div");
-        card.classList.add("list-card");
-        if (filter.icon){
-            const icon = document.createElement("img");
-            icon.src = filter.icon;
-            card.appendChild(icon);
-        }
-
-        card.appendChild(document.createTextNode(filter.name));
+        const card = makeCard.list(filter);
     
         cardContainer.appendChild(card)
     }
@@ -23,7 +17,7 @@ const sidebarElement = document.querySelectorAll("#sidebar > div")
 
 export const sidebar = function (){
 
-    function display(defaultLists, ...OtherLists){
+    const display = (defaultLists, ...OtherLists) => {
         clean()
         for (let list of defaultLists) {
             const div = document.createElement("div");
@@ -54,7 +48,7 @@ export const sidebar = function (){
         }
      }
     
-     function clean() {
+     const clean = () => {
         for (let list of sidebarElement) {
             while (list.children.length > 1) {
                 list.removeChild(list.lastChild);
@@ -64,4 +58,102 @@ export const sidebar = function (){
  
 
     return { display }
+}()
+
+const makeCard = function (){
+    const list = (filter) => {
+        const card = document.createElement("div");
+        card.classList.add("list-card");
+        if (filter.icon){
+            const icon = document.createElement("img");
+            icon.src = filter.icon;
+            card.appendChild(icon);
+        }
+
+        card.appendChild(document.createTextNode(filter.name));
+        return card;
+    } 
+
+    const task = (task) => {
+        const card = document.createElement("div");
+        card.classList.add("task-card");
+
+        const title = document.createElement("h1");
+        title.textContent = task.title;
+        title.classList.add("title")
+        card.appendChild(title);
+        
+        const priority = document.createElement("div");
+        priority.textContent = task.priority;
+        priority.classList.add(`priority ${task.priority}`);
+        card.appendChild(priority);
+
+        const description = document.createElement("p");
+        description.textContent = task.description;
+        card.appendChild(description);
+
+        const dueDate = document.createElement("div");
+        dueDate.textContent = `Due: ${task.dueDate}`;
+        dueDate.classList.add("date");
+        card.appendChild(dueDate);
+
+        const controls = document.createElement("div");
+        controls.classList.add("controls");
+
+        for (let icon in Task.icons) {
+
+            const button = document.createElement("button");
+            button.classList.add(`${icon}`);
+            button.setAttribute(type, "button");
+
+            const img = document.createElement("img");
+            img.setAttribute("width", "20");
+            img.setAttribute("height", "20");
+            img.src = Task.icons[icon];
+            
+            button.appendChild(img);
+
+            controls.appendChild(button);
+        }
+
+        card.appendChild(controls);
+    }
+
+    const note = (note) => {
+        const card = document.createElement("div");
+        card.classList.add("task-card");
+
+        const title = document.createElement("h1");
+        title.textContent = note.title;
+        title.classList.add("title")
+        card.appendChild(title);
+
+        const description = document.createElement("p");
+        description.textContent = note.description;
+        card.appendChild(description);
+
+
+        const controls = document.createElement("div");
+        controls.classList.add("controls");
+
+        for (let icon in Note.icons) {
+
+            const button = document.createElement("button");
+            button.classList.add(`${icon}`);
+            button.setAttribute(type, "button");
+
+            const img = document.createElement("img");
+            img.setAttribute("width", "20");
+            img.setAttribute("height", "20");
+            img.src = Note.icons[icon];
+            
+            button.appendChild(img);
+
+            controls.appendChild(button);
+        }
+
+        card.appendChild(controls)
+    }
+
+    return { list, task, note }
 }()
